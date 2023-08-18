02f582326b12d3ef9427e008a5964e7cc6bab80f
Demis Bellot <demis.bellot@gmail.com>
2023-08-18 13:27:36 +0800

    Remove registering/resolving deps in AppTasks

--- CoffeeShop/Configure.AppHost.cs
+++ CoffeeShop/Configure.AppHost.cs
@@ -15,24 +15,24 @@ public class AppHost : AppHostBase, IHostingStartup
     public void Configure(IWebHostBuilder builder) => builder
         .ConfigureServices((context,services) => {
             // Configure ASP.NET Core IOC Dependencies
-            var appConfig = new AppConfig {
-                NodePath = ScriptContext.ProtectedMethods.exePath("node")
-                    ?? throw new Exception("Could not resolve path to node")
-            };
+            var appConfig = new AppConfig();
             context.Configuration.Bind(nameof(AppConfig), appConfig);
             services.AddSingleton(appConfig);
 
             if (!AppTasks.IsRunAsAppTask())
             {
+                appConfig.NodePath = ScriptContext.ProtectedMethods.exePath("node")
+                                     ?? throw new Exception("Could not resolve path to node");
+
                 var googleCredentials = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
                 if (string.IsNullOrEmpty(googleCredentials))
                     throw new Exception("GOOGLE_APPLICATION_CREDENTIALS Environment Variable not set");
                 if (!File.Exists(googleCredentials))
                     throw new Exception($"GOOGLE_APPLICATION_CREDENTIALS '{googleCredentials}' does not exist");
-            }
 
-            services.AddSingleton(SpeechClient.Create());
-            services.AddSingleton(StorageClient.Create());
+                services.AddSingleton(SpeechClient.Create());
+                services.AddSingleton(StorageClient.Create());
+            }
         });
 
     public AppHost() : base("CoffeeShop", typeof(MyServices).Assembly) {}
@@ -46,15 +46,18 @@ public class AppHost : AppHostBase, IHostingStartup
             "http://localhost:5173", //vite dev
         }, allowCredentials:true));
 
-        var appConfig = container.Resolve<AppConfig>();
-        var wwwrootVfs = GetVirtualFileSource<FileSystemVirtualFiles>();
-        var googleCloudVfs = new GoogleCloudVirtualFiles(container.Resolve<StorageClient>(), appConfig.CoffeeShop.Bucket);
-        Plugins.Add(new FilesUploadFeature(
-            new UploadLocation("products", wwwrootVfs, allowExtensions:FileExt.WebImages,
-                resolvePath: ctx => $"/products/{ctx.FileName}"),
-            new UploadLocation("recordings", googleCloudVfs, allowExtensions:FileExt.WebAudios, writeAccessRole: RoleNames.AllowAnon,
-                maxFileBytes: 1024 * 1024,
-                resolvePath: ctx => $"/recordings/{ctx.DateSegment}/{DateTime.UtcNow.TimeOfDay.TotalMilliseconds}.{ctx.FileExtension}")
+        if (!AppTasks.IsRunAsAppTask())
+        {
+            var appConfig = container.Resolve<AppConfig>();
+            var wwwrootVfs = GetVirtualFileSource<FileSystemVirtualFiles>();
+            var googleCloudVfs = new GoogleCloudVirtualFiles(container.Resolve<StorageClient>(), appConfig.CoffeeShop.Bucket);
+            Plugins.Add(new FilesUploadFeature(
+                new UploadLocation("products", wwwrootVfs, allowExtensions:FileExt.WebImages,
+                    resolvePath: ctx => $"/products/{ctx.FileName}"),
+                new UploadLocation("recordings", googleCloudVfs, allowExtensions:FileExt.WebAudios, writeAccessRole: RoleNames.AllowAnon,
+                    maxFileBytes: 1024 * 1024,
+                    resolvePath: ctx => $"/recordings/{ctx.DateSegment}/{DateTime.UtcNow.TimeOfDay.TotalMilliseconds}.{ctx.FileExtension}")
             ));
+        }
     }
 }
