bf8161334c146fd6595ec80155958d4e6c5f3031
Demis Bellot <demis.bellot@gmail.com>
2023-09-14 02:29:17 +0800

    Update RecordButton.mjs

--- CoffeeShop/wwwroot/mjs/components/RecordButton.mjs
+++ CoffeeShop/wwwroot/mjs/components/RecordButton.mjs
@@ -12,7 +12,7 @@ export default {
            <svg class="w-8 h-8 lg:w-10 lg:h-10 text-white" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"><g fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="4"><rect width="14" height="27" x="17" y="4" rx="7"/><path stroke-linecap="round" d="M9 23c0 8.284 6.716 15 15 15c8.284 0 15-6.716 15-15M24 38v6"/></g></svg>
        </div>
    </div>`,
-    emits:['click'],
+    emits:['toggle'],
     props: { loading:Boolean, recording:Boolean },
     setup(props, { emit }) {
         const css = `
