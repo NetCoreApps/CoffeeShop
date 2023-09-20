b003dab1e3e739a97bf259d417612e18776a65de
Demis Bellot <demis.bellot@gmail.com>
2023-09-20 14:17:01 +0800

    Update Configure.Speech.cs

--- CoffeeShop/Configure.Speech.cs
+++ CoffeeShop/Configure.Speech.cs
@@ -4,9 +4,8 @@ using ServiceStack.AI;
 using ServiceStack.IO;
 using ServiceStack.GoogleCloud;
 using Google.Cloud.Speech.V2;
-using CoffeeShop.ServiceInterface;
-using CoffeeShop.ServiceModel;
 using Microsoft.CognitiveServices.Speech;
+using CoffeeShop.ServiceInterface;
 
 [assembly: HostingStartup(typeof(CoffeeShop.ConfigureSpeech))]
 
@@ -25,38 +24,36 @@ public class ConfigureSpeech : IHostingStartup
                 GoogleCloudConfig.AssertValidCredentials();
                 services.AddSingleton<ISpeechToText>(c => {
                     var config = c.Resolve<AppConfig>();
-                    var siteConfig = config.CoffeeShop;
-                    var gcpConfig = config.AssertGcpConfig();
-                    return new GoogleCloudSpeechToText(
-                        SpeechClient.Create(),
+                    var google = config.AssertGcpConfig();
+                    return new GoogleCloudSpeechToText(SpeechClient.Create(),
                         new GoogleCloudSpeechConfig {
-                            Project = gcpConfig.Project,
-                            Location = gcpConfig.Location,
-                            Bucket = gcpConfig.Bucket,
-                            RecognizerId = siteConfig.RecognizerId,
-                            PhraseSetId = siteConfig.PhraseSetId,
+                            Project = google.Project,
+                            Location = google.Location,
+                            Bucket = google.Bucket,
+                            RecognizerId = config.CoffeeShop.RecognizerId,
+                            PhraseSetId = config.CoffeeShop.PhraseSetId,
                         }
                     );
                 });
             }
             else if (speechProvider == nameof(AwsSpeechToText))
             {
-                services.AddSingleton(c => X.Map(c.Resolve<AppConfig>().AssertAwsConfig(), x => 
-                    new AmazonTranscribeServiceClient(x.AccessKey, x.SecretKey, RegionEndpoint.GetBySystemName(x.Region)))!);
-                
-                services.AddSingleton<ISpeechToText>(c => new AwsSpeechToText(
-                    c.Resolve<AmazonTranscribeServiceClient>(),
-                    new AwsSpeechToTextConfig {
-                        Bucket = c.Resolve<AppConfig>().AssertAwsConfig().Bucket,
-                        VocabularyName = c.Resolve<AppConfig>().CoffeeShop.VocabularyName,
-                    }));
+                services.AddSingleton<ISpeechToText>(c => {
+                    var config = c.Resolve<AppConfig>();
+                    var aws = config.AssertAwsConfig();
+                    return new AwsSpeechToText(new AmazonTranscribeServiceClient(
+                            aws.AccessKey, aws.SecretKey, RegionEndpoint.GetBySystemName(aws.Region)),
+                        new AwsSpeechToTextConfig {
+                            Bucket = aws.Bucket,
+                            VocabularyName = config.CoffeeShop.VocabularyName,
+                        });
+                });
             }
             else if (speechProvider == nameof(AzureSpeechToText))
             {
                 services.AddSingleton<ISpeechToText>(c => {
-                    var config = c.Resolve<AppConfig>();
-                    var azureConfig = config.AssertAzureConfig();
-                    var speechConfig = SpeechConfig.FromSubscription(azureConfig.SpeechKey, azureConfig.SpeechRegion);
+                    var azure = c.Resolve<AppConfig>().AssertAzureConfig();
+                    var speechConfig = SpeechConfig.FromSubscription(azure.SpeechKey, azure.SpeechRegion);
                     speechConfig.SpeechRecognitionLanguage = "en-US";
                     return new AzureSpeechToText(speechConfig);
                 });
